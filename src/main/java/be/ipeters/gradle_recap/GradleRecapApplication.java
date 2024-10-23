e940d66c3a2412f95b896947e050ff00f9ae14d8
Carl Peters <31207893+cpjjpeters@users.noreply.github.com>
2024-10-23 18:19:48 +0200

    gradle branch is main

---
+++ src/main/java/be/ipeters/gradle_recap/GradleRecapApplication.java
@@ -0,0 +1,13 @@
+package be.ipeters.gradle_recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleRecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ipeters/gradle_recap/GradleRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ipeters.gradle_recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
